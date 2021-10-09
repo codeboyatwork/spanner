e2b34083ae17ac641f1ce02852636edeef7cc96c
Tanmay <tanmaydeshmukh190@hotmail.com>
2021-10-09 19:53:51 +0530

    Initial commit

---
+++ src/test/java/spanner_demo/spanner/Launcher.java
@@ -0,0 +1,12 @@
+package spanner_demo.spanner;
+
+import junit.framework.TestCase;
+
+public class Launcher extends TestCase {
+	public static final String INSTANCE_ID = "my-spanner-td";
+	public static final String DATABASE_ID = "demo_spanner";
+
+	public void test_spanner() throws Exception {
+		App.main(INSTANCE_ID, DATABASE_ID);
+	}
+}
