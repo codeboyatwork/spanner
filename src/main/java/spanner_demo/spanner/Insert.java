65c44205d68f263ee874e972b630f883ca52cdf0
Tanmay <tanmaydeshmukh190@hotmail.com>
2021-10-09 21:49:01 +0530

    summary

--- src/main/java/spanner_demo/spanner/Insert.java
+++ src/main/java/spanner_demo/spanner/Insert.java
@@ -11,9 +11,10 @@ import com.google.cloud.spanner.Statement;
  * A quick start code for Cloud Spanner. It demonstrates how to setup the Cloud Spanner client and
  * execute a simple query using it against an existing database.
  */
-public class App {
+public class Insert {
   public static void main(String... args) throws Exception {
-
+	long keyvalue = 0;
+    String nametouse = "";
     if (args.length != 2) {
       System.err.println("Usage: QuickStartSample <instance_id> <database_id>");
       return;
@@ -29,15 +30,29 @@ public class App {
       // Creates a database client
       DatabaseClient dbClient =
           spanner.getDatabaseClient(DatabaseId.of(options.getProjectId(), instanceId, databaseId));
+      
       // Queries the database
-      try (ResultSet resultSet = dbClient.singleUse().executeQuery(Statement.of("SELECT * FROM my_table"))) {
-        System.out.println("\n\nResults:");
-        // Prints the results
-        while (resultSet.next()) {
-          System.out.printf("%d ", resultSet.getLong(0));
-          System.out.printf("%s\n", resultSet.getString(1));
-        }
-      }
+      try (ResultSet resultSet = dbClient.singleUse().executeQuery(Statement.of("select MAX(id_column) from my_table"))) {
+          System.out.println("\n\nResults:");
+          // Prints the results
+          while (resultSet.next()) {
+            keyvalue = resultSet.getLong(0);
+            keyvalue += 1;
+            nametouse = "name" + keyvalue;
+          }
+        }  
+      final long tempkey = keyvalue;
+      final String tempname = nametouse;
+      dbClient
+      .readWriteTransaction()
+      .run(transaction -> {
+        String sql =
+            "INSERT INTO my_table (id_column, name_column) "
+                + " VALUES ("+tempkey+",'"+tempname+"')";
+        long rowCount = transaction.executeUpdate(Statement.of(sql));
+        System.out.printf("%d record inserted.\n", rowCount);
+        return null;
+      });
     } finally {
       // Closes the client which will free up the resources used
       spanner.close();
--- src/test/java/spanner_demo/spanner/LauncherTest.java
+++ src/test/java/spanner_demo/spanner/LauncherTest.java
@@ -6,7 +6,10 @@ public class LauncherTest extends TestCase {
 	public static final String INSTANCE_ID = "my-spanner-td";
 	public static final String DATABASE_ID = "demo_spanner";
 
-	public void test_spanner() throws Exception {
+	public void test_select() throws Exception {
 		App.main(INSTANCE_ID, DATABASE_ID);
+	}
+	public void test_insert() throws Exception {
+		Insert.main(INSTANCE_ID, DATABASE_ID);
 	}
 }
